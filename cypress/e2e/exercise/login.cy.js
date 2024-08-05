f6743aa52223ca5884a3ca93ed3fabc5394078be
Jorge Teruya <jorgemkazut@gmail.com>
2024-08-05 16:33:50 -0300

    Fixed validation message, the user name was wrong

--- cypress/e2e/exercise/login.cy.js
+++ cypress/e2e/exercise/login.cy.js
@@ -1,4 +1,4 @@
-//// <refence type="cypress"/>
+// <refence type="cypress"/>
 
 describe('Funcionalidade: Login', () => { 
 
@@ -12,20 +12,20 @@ describe('Funcionalidade: Login', () => {
         cy.get('#password').type('teste@124')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, teste-8790 (não é teste-8790? Sair)')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, teste-8697 (não é teste-8697? Sair)')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {  
-        cy.get('#username').type('tester@124')
+        cy.get('#username').type('tester@124.com')
         cy.get('#password').type('teste@124')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido.')       
+        cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido')       
     });
 
     it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         
-        cy.get('#username').type('teste@124')
+        cy.get('#username').type('teste@124.com')
         cy.get('#password').type('senha_errada')
         cy.get('.woocommerce-form > .button').click()
 
