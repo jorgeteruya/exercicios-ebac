0fae71b40c9d19d5d81a995703f0087f736c8e09
Jorge <jorgemkazut@gmail.com>
2024-08-05 23:17:10 -0300

    Add scenarios on produtos.cy.js

--- cypress/e2e/exercise/produtos.cy.js
+++ cypress/e2e/exercise/produtos.cy.js
@@ -10,4 +10,16 @@ describe('Funcionalidade: Produtos', () => {
             .first()
             .click()
     });
+
+    it('Deve selecionar um outro produto da lista', () => {
+        cy.get('.product-block')
+            .eq(2)
+            .click()
+    });
+
+    it('Deve selecionar o produto Apollo Running Short', () => {
+        cy.get('.product-block')
+            .contains('Apollo Running Short')
+            .click()
+    });
 })
